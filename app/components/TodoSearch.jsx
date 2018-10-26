304c6e4f6a522facc71a174556933ee6108b6550
rdollent <rumbalumba89@gmail.com>
2018-10-25 23:32:45 -0600

    unfinished TodoSearch

--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -5,11 +5,24 @@ class TodoSearch extends React.Component {
         super(props);
     }
 
+    handleSearch = () => {
+        // if box is checked, true. if not, false.
+        const showCompleted = this.refs.showCompleted.checked;
+        const searchText = this.refs.searchText.value;
+
+        
+    }
     render = () => {
         return (
             <div>
-                from c9!
-                from local!
+                <div>
+                    <input type="search" ref="searchText" placeholder="search todos" onChange={this.handleSearch}/>
+                </div>
+                <div>
+                    <label htmlFor="">
+                        <input type="checkbox" ref="showCompleted" onChange={this.handleSearch}/>
+                    </label>
+                </div>
             </div>
 
         )
