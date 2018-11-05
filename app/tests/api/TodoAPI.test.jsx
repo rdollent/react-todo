df40875efc27be95e6d5013fce464befa024b47c
rdollent <rumbalumba89@gmail.com>
2018-11-05 00:47:47 -0700

    todoapi test started

---
+++ app/tests/api/TodoAPI.test.jsx
@@ -0,0 +1,19 @@
+const expect = require('expect');
+
+const TodoAPI = require('TodoAPI');
+
+describe('TodoAPI', () => {
+    it('should exist', () => {
+        expect(TodoAPI).toExist();
+    });
+
+    // describe block for setTodo
+    describe('setTodos', () => {
+
+    });
+
+    describe('getTodos', () => {
+
+    });
+
+})
