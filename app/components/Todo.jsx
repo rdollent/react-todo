4219f41f65df9128afc228e1c6568440c9730113
Rod <rumbalumba89@gmail.com>
2018-11-14 22:36:34 +0000

    added completedAt and createdAt timestamps for todos, and tests

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -11,15 +11,22 @@ class Todo extends React.Component {
         // which means for every todo
         // there are id and text object props in this.props
         // use es6 destructuring
-        const {id, text, completed, createdAt} = this.props;
+        const {id, text, completed, createdAt, completedAt} = this.props;
         
         // createdAt is stored in unix time
         // need to convert for user
         const renderDate = () => {
-            const message = 'Created ';
-            const timestamp = createdAt;
+            let message = 'Created ';
+            let timestamp = createdAt;
+            
+            if(completed) {
+                message = 'Completed ';
+                timestamp = completedAt;
+                
+            }
+            
             return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
-        }
+        };
         return (
             <div onClick={() => {
                 this.props.onToggle(id);
@@ -28,7 +35,7 @@ class Todo extends React.Component {
                 <p>{text}</p>
                 <p>{renderDate()}</p>
             </div>
-        )
+        );
     }
 }
 
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -25,14 +25,18 @@ describe('TodoApp', () => {
         // we now expect it to only contain the new todo
         expect(todoApp.state.todos[0].text).toBe(todoText);
 
-
+        // expect createdAt field to be a number
+        expect(todoApp.state.todos[0].createdAt).toBeA('number');
+        
     });
     
     it('should toggle completed value when handleToggle called', () => {
         const todoData = {
             id: 11,
             text: 'test features',
-            completed: false
+            completed: false,
+            createdAt: 0,
+            completedAt: undefined
         };
         
         const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
@@ -40,14 +44,41 @@ describe('TodoApp', () => {
         // replace your todos state with test state data
         todoApp.setState({todos: [todoData]});
        
-       // check that todos first iteam has completed value of false
-       expect(todoApp.state.todos[0].completed).toBe(false);
-       // call handleToggle with id (11);
-       todoApp.handleToggle(11);
-       // verify that value changed
-       expect(todoApp.state.todos[0].completed).toBe(true);
-        
-        
+        // check that todos first iteam has completed value of false
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        // call handleToggle with id (11);
+        todoApp.handleToggle(11);
+        // verify that value changed
+        expect(todoApp.state.todos[0].completed).toBe(true);
+    
+        // expect completedAt to be a number
+        expect(todoApp.state.todos[0].completedAt).toBeA('number');
+    
     });
 
+    // test that when completed status is toggled from true to false, completedAt gets removed
+    it('should remove completedAt when completed is false', () => {
+        const todoData = {
+            id: 11,
+            text: 'test features',
+            completed: true,
+            createdAt: 0,
+            completedAt: 123
+        };
+        
+        const todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+        
+        // replace your todos state with test state data
+        todoApp.setState({todos: [todoData]});
+       
+        // check that todos first iteam has completed value of false
+        expect(todoApp.state.todos[0].completed).toBe(true);
+        // call handleToggle with id (11);
+        todoApp.handleToggle(11);
+        // verify that value changed
+        expect(todoApp.state.todos[0].completed).toBe(false);
+    
+        // expect completedAt to be undefined
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
+    });
 });
